9226ed8785b198ee25defcbd0dcf17f566cbe90b
dabby9734 <dabby9734@users.noreply.github.com>
2022-02-03 09:14:42 +0000

    deploy: cc9439e849ecd2e06a1e575dbd16beaa6e680bd5

--- node_modules/@redocly/openapi-core/lib/decorators/oas3/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Oas3Decorator } from '../../visitors';
-export declare const decorators: {
-    'registry-dependencies': Oas3Decorator;
-    'operation-description-override': Oas3Decorator;
-    'tag-description-override': Oas3Decorator;
-    'info-description-override': Oas3Decorator;
-    'remove-x-internal': Oas3Decorator;
-};
--- node_modules/redoc-cli/node_modules/@redocly/openapi-core/lib/decorators/oas2/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Oas2Decorator } from '../../visitors';
-export declare const decorators: {
-    'registry-dependencies': Oas2Decorator;
-    'operation-description-override': Oas2Decorator;
-    'tag-description-override': Oas2Decorator;
-    'info-description-override': Oas2Decorator;
-    'remove-x-internal': Oas2Decorator;
-};
