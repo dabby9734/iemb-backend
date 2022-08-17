541d0cd778ea883dd525ae5d019a01b9531e40d0
Liu Weichu <weichufmsp@gmail.com>
2022-08-17 20:50:29 +0800

    fix: many many bugs in star endpoint

--- star/index.js
+++ star/index.js
@@ -23,7 +23,9 @@ module.exports = async function (context, req) {
     };
   }
 
-  const postData = `status=${status}&boardId=${bid}&topicid=${pid}`;
+  const postData = `status=${
+    status === "true" ? 1 : 0
+  }&boardId=${bid}&topicid=${pid}`;
   const response = await fetch("https://iemb.hci.edu.sg/Board/ProcessFav", {
     method: "POST",
     headers: {
@@ -32,7 +34,7 @@ module.exports = async function (context, req) {
         "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36",
       referer: `https://iemb.hci.edu.sg/Board/content/${pid}?board=${pid}&isArchived=False`,
       "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-      cookie: `__RequestVerificationToken=LzTV_NzJhbaNwRaOaIrEcDy2bXy-dv4nLjpOSB4tw-DxCVTViiwDdy8xSlYoQNf8ZuSfzybs-wQC45UxTLZWcjsYbKvO3Xro3JlFBPH8hMw1; ASP.NET_SessionId=ypcaghdk0m45dvrxmk4ha3de; AuthenticationToken=51d2a570-3bf5-4ce4-9ca3-e10b9b471fa6`,
+      cookie: `__RequestVerificationToken=${veriToken};ASP.NET_SessionId=${sessionID}; AuthenticationToken=${authToken}`,
     },
     redirect: "manual",
     body: postData,
